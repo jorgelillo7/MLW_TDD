2ce5d6be69d00ec7cbe00f1a494bcf0654f58c43
Jorge Lillo <jorge.lillo9@gmail.com>
2015-04-16 00:10:39 +0200

    Cambios en estructura del proyecto para mejorar su legibilidad

---
+++ test/klondike/AllTest.java
@@ -0,0 +1,11 @@
+package klondike;
+ 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({MoveDeckToWasteControllerTest.class, StartGameControllerTest.class})
+public class AllTest {
+
+}
