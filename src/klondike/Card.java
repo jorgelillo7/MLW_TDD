19b4b88664d4fcb2f92734c316b28271f20516a3
Jorge Lillo <jorge.lillo9@gmail.com>
2015-04-17 11:11:16 +0200

    Modified Card model to acept value and suit

--- src/klondike/Card.java
+++ src/klondike/Card.java
@@ -2,9 +2,23 @@ package klondike;
 
 public class Card {
 
-	public boolean uncovered() {
-		return true;
-		
+	private boolean uncovered;
+	private int value;
+	private int suit;
+	
+	public Card (int value, int suit){
+		this.value = value;
+		this.suit = suit;
 	}
+	
+	public boolean isUncovered() {
+		return uncovered;
+	}
+
+	public void setUncovered(boolean uncovered) {
+		this.uncovered = uncovered;
+	}
+	
+	
 
 }
